9c6d0975331a94807b0c9192b2bace8603637632
Tomasz Stachowiak <h3@h3.gd>
2020-01-31 20:51:54 +0100

    :chart_with_upwards_trend::chart_with_upwards_trend::chart_with_upwards_trend:

---
+++ Assets/Scripts/ClicketyHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClicketyHandler : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            Debug.Log ("ohai there");
+        }
+    }
+}
