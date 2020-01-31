1c13e05961f9e32aa2b519054cc0436453ae4c10
Tomasz Stachowiak <h3@h3.gd>
2020-01-31 20:54:19 +0100

    :poop::mouse2:

--- Assets/Scripts/ClicketyHandler.cs
+++ Assets/Scripts/ClicketyHandler.cs
@@ -11,7 +11,7 @@ public class ClicketyHandler : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if (Input.GetMouseButtonDown (0)) {
-            Debug.Log ("ohai there");
+            Debug.Log ("pearfect click!");
         }
     }
 }
