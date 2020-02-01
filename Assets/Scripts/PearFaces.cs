e76468a42d4466741cd9169fb47b97774877a9ca
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-01 11:15:02 +0100

    :clown_face:

---
+++ Assets/Scripts/PearFaces.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PearFaces : MonoBehaviour {
+
+    public Mesh defaultFace;
+    public Mesh pearsistentFace;
+    float cooldown = 0.3f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        this.GetComponent<MeshFilter>().mesh = defaultFace;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            this.GetComponent<MeshFilter>().mesh = pearsistentFace;
+            //this.cooldown = 0.3f;
+        } else {
+            this.GetComponent<MeshFilter>().mesh = defaultFace;
+        }
+
+        //this.cooldown -= Time.deltaTime;
+    }
+}
