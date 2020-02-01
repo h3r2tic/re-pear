62c2c4dee78cce754a0f81e3290cae9047f889eb
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-01 17:53:24 +0100

    :checkered_flag:

--- Assets/Scripts/RaceMode.cs
+++ Assets/Scripts/RaceMode.cs
@@ -6,11 +6,22 @@ using UnityEngine.UI;
 public class RaceMode : MonoBehaviour {
 
     public float timer = 20.0f; //20sec
+    public float pretimer = 1.0f;
 
     MeasureTravelDistance watch;
     Text countdown;
 
     bool isActive = false;
+
+    GameObject stopwatch;
+
+    void Start() {
+        this.stopwatch = GameObject.Find("Stopwatch");
+        Debug.Log(this.stopwatch);
+
+        this.countdown = GameObject.Find("Countdown").GetComponent<Text>();
+        this.stopwatch.SetActive(false);
+    }
 
     public void StartRace() {
         this.isActive = true;
@@ -22,25 +33,34 @@ public class RaceMode : MonoBehaviour {
         watch.Go();
         //
         this.timer = 20.0f;
+        this.pretimer = 3.0f;
         //expand the floor
         var ground = GameObject.Find("Ground");
         ground.transform.localScale = new Vector3(30.0f, 1.0f, 30.0f);
         //
-        this.countdown = GameObject.Find("Countdown").GetComponent<Text>();
+        GameObject.Find("StartText").GetComponent<Text>().enabled = true;
+
     }
 
     void Update() {
 
         if (this.isActive) {
 
-            this.timer -= Time.deltaTime;
-            this.countdown.text = this.timer.ToString("0.0");
-            Debug.Log("Workig!");
+            if (this.pretimer > 0.0f) {
+                this.pretimer -= Time.deltaTime;
+            } else {
 
-            if (this.timer < 0.0f) {
-                this.watch.Stop();
-                //Display total distance traveled at the end
-                this.isActive = false;
+                GameObject.Find("StartText").GetComponent<Text>().enabled = false;
+                this.stopwatch.SetActive(true);
+
+                this.timer -= Time.deltaTime;
+                this.countdown.text = this.timer.ToString("0.0");
+
+                if (this.timer < 0.0f) {
+                    this.watch.Stop();
+                    //Display total distance traveled at the end
+                    this.isActive = false;
+                }
             }
         }
 
