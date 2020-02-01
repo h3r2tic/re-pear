41f48834929f315ef0cc05598d2110e64ca06eeb
Tomasz Stachowiak <h3@h3.gd>
2020-02-01 17:27:57 +0100

    :skull::sound:

---
+++ Assets/Scripts/LetTheBodiesHitTheFloor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetTheBodiesHitTheFloor : MonoBehaviour {
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+
+    private void playDeathScream(GameObject obj) {
+        var ds = obj.GetComponent<DeathScream>();
+        if (ds && ds.clip) {
+            this.audioSource.PlayOneShot(ds.clip);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.transform) {
+            var obj = collision.transform;
+            playDeathScream(obj.gameObject);
+
+            while (obj.parent) {
+                obj = obj.parent;
+                playDeathScream(obj.gameObject);
+            }
+
+            Destroy(obj.gameObject);
+        }
+    }
+}
