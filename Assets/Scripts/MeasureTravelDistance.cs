fccc26eea927656e8b80c92f09331b896078f975
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-01 16:54:51 +0100

    :checkered_flag:

---
+++ Assets/Scripts/MeasureTravelDistance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureTravelDistance : MonoBehaviour {
+    private float distanceTraveled = 0.0f;
+    private bool isActive = false;
+
+    Transform centerOfMass;
+    Vector3 prevPos;
+
+    public void Go() {
+        this.isActive = true;
+        this.distanceTraveled = 0.0f;
+        this.centerOfMass = this.transform.GetChild(0).transform;
+        this.prevPos = centerOfMass.position;
+    }
+
+    public float Stop() {
+        this.isActive = false;
+        return this.distanceTraveled;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.isActive) {
+            this.distanceTraveled += Vector3.Distance(centerOfMass.position, this.prevPos);
+            this.prevPos = centerOfMass.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            this.isActive = !this.isActive;
+        }
+    }
+}
---
+++ Assets/Scripts/RaceMode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceMode : MonoBehaviour {
+
+    public float timer = 20.0f; //20sec
+
+    MeasureTravelDistance watch;
+
+    public void StartRace() {
+        //remove ability to move objects
+        FindObjectsOfType<ClicketyHandler>()[0].enabled = false;
+        Cursor.visible = false;
+        //
+        this.watch = FindObjectsOfType<MeasureTravelDistance>()[0];
+        watch.Go();
+        //
+        this.timer = 20.0f;
+    }
+
+    void Update() {
+        this.timer -= Time.deltaTime;
+
+        if (this.timer < 0.0f) {
+            this.watch.Stop();
+            //Display total distance traveled at the end
+        }
+    }
+
+}
