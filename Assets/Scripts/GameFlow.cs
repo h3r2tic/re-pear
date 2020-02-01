9a55d5aee797cd2f1deca332b6a54786d8a93870
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-01 18:06:49 +0100

    :checkered_flag:

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameFlow : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.R)) {
+            Cursor.visible = true;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
