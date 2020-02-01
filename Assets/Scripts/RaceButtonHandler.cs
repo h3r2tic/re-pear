ed3a8b408016f7c872730fa111e5d912ae154757
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-01 17:33:13 +0100

    :checkered_flag:

---
+++ Assets/Scripts/RaceButtonHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceButtonHandler : MonoBehaviour {
+    // Start is called before the first frame update
+    public void StartRace() {
+        FindObjectsOfType<RaceMode>()[0].StartRace();
+    }
+
+}
--- Assets/Scripts/RaceMode.cs
+++ Assets/Scripts/RaceMode.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RaceMode : MonoBehaviour {
 
     public float timer = 20.0f; //20sec
 
     MeasureTravelDistance watch;
+    Text countdown;
+
+    bool isActive = false;
 
     public void StartRace() {
+        this.isActive = true;
         //remove ability to move objects
         FindObjectsOfType<ClicketyHandler>()[0].enabled = false;
         Cursor.visible = false;
@@ -17,15 +22,28 @@ public class RaceMode : MonoBehaviour {
         watch.Go();
         //
         this.timer = 20.0f;
+        //expand the floor
+        var ground = GameObject.Find("Ground");
+        ground.transform.localScale = new Vector3(30.0f, 1.0f, 30.0f);
+        //
+        this.countdown = GameObject.Find("Countdown").GetComponent<Text>();
     }
 
     void Update() {
-        this.timer -= Time.deltaTime;
 
-        if (this.timer < 0.0f) {
-            this.watch.Stop();
-            //Display total distance traveled at the end
+        if (this.isActive) {
+
+            this.timer -= Time.deltaTime;
+            this.countdown.text = this.timer.ToString("0.0");
+            Debug.Log("Workig!");
+
+            if (this.timer < 0.0f) {
+                this.watch.Stop();
+                //Display total distance traveled at the end
+                this.isActive = false;
+            }
         }
+
     }
 
 }
