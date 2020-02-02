7962d4c03f2820eacf25a9ed26f6831548b3b192
Ari Vaniderstine <ari.vaniderstine@embark-studios.com>
2020-02-02 14:38:44 +0100

    WebGL

--- Assets/Scripts/EndGameButtonHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EndGameButtonHandler : MonoBehaviour {
-    public void CallRestartGame() {
-        FindObjectsOfType<GameFlow>()[0].ReloadCurrentScene();
-    }
-}
