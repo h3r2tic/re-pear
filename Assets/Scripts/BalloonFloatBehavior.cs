e35f7680e23640cea9f82632cf3a5c8b79afcc9f
Tomasz Stachowiak <h3@h3.gd>
2020-02-02 14:13:14 +0100

    balloon

---
+++ Assets/Scripts/BalloonFloatBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controllable))]
+public class BalloonFloatBehavior : MonoBehaviour {
+    private Controllable controllable;
+    public Rigidbody floatyBit;
+    public Rigidbody baseBit;
+    public float floatForce = 1.0f;
+
+    void Start() {
+        controllable = GetComponent<Controllable>();
+    }
+
+    void Update() {
+        if (!floatyBit || !baseBit || !controllable) {
+            return;
+        }
+
+        baseBit.mass = controllable.connectionCount > 0 ? 0.25f : 1.0f;
+
+        floatyBit.AddForce(new Vector3(0.0f, floatForce, 0.0f), ForceMode.Force);
+    }
+}
