739304a45ba707e6f1d11047eb414272a29c874e
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-02 10:57:24 +0100

    restart level if pear is deleted:

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameFlow : MonoBehaviour {
+
+    float countdown = 1.0f;
+    bool isDelayedRestart = false;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.R)) {
             FindObjectsOfType<RaceMode>()[0].ClearEndStateUI();
             ReloadCurrentScene();
+        }
+
+        if (this.isDelayedRestart) {
+            this.countdown -= Time.deltaTime;
+            if (this.countdown < 0.0f) {
+                this.isDelayedRestart = false;
+                this.ReloadCurrentScene();
+            }
         }
     }
 
@@ -20,5 +32,10 @@ public class GameFlow : MonoBehaviour {
         Cursor.visible = true;
         FindObjectsOfType<RaceMode>()[0].ClearEndStateUI();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void DelayedRestart() {
+        this.isDelayedRestart = true;
+        this.countdown = 1.0f;
     }
 }
