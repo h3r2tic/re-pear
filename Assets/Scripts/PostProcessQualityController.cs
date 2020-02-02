d22513468ff326fdcbcc560b87656b25f7e897b0
Tomasz Stachowiak <h3@h3.gd>
2020-02-02 01:12:27 +0100

    :racehorse::racehorse::racehorse::metal::metal::metal::metal:

---
+++ Assets/Scripts/PostProcessQualityController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[RequireComponent(typeof(PostProcessVolume))]
+public class PostProcessQualityController : MonoBehaviour {
+    public List<PostProcessProfile> profiles = new List<PostProcessProfile>();
+
+    // Start is called before the first frame update
+    void Start() {
+        int qualityLevel = QualitySettings.GetQualityLevel();
+        Debug.Log("current quality level: " + qualityLevel);
+
+        if (qualityLevel < 2) {
+            var ssrt = GetComponent<SSRT>();
+            if (ssrt) {
+                ssrt.enabled = false;
+            }
+        }
+
+        GetComponent<PostProcessVolume>().profile = this.profiles[qualityLevel];
+    }
+}
