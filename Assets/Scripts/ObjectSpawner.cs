cb1e87a0f4d26bd6ed583d8409e199788851dd19
Anastasia Opara <anastasia.opara@embark-studios.com>
2020-02-02 13:01:35 +0100

    better rng distribution of object spawner

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -12,6 +12,9 @@ public class ObjectSpawner : MonoBehaviour {
     public int disconnectedCount = 0;
     int totalSpawnCount = 0;
 
+    //
+    private List<int> randomSequence = new List<int>();
+
     public static ObjectSpawner instance;
     void Awake() {
         instance = this;
@@ -35,7 +38,7 @@ public class ObjectSpawner : MonoBehaviour {
 
         Vector3 pos = new Vector3(Random.Range(-spawnerExtents.x, spawnerExtents.x), 0.0f, Random.Range(-spawnerExtents.y, spawnerExtents.y)) + this.offset;
 
-        int idx = Random.Range(0, prefabs.Count);
+        int idx = GetNewRandom(); //Random.Range(0, prefabs.Count);
 
         // Only spawn those once we have spawned at least three other objects
         while (prefabs[idx].GetComponent<WutBehavior>() && totalSpawnCount <= 3) {
@@ -46,5 +49,36 @@ public class ObjectSpawner : MonoBehaviour {
 
         ++totalSpawnCount;
         return Instantiate(prefab, pos, prefab.transform.localRotation).gameObject;
+    }
+
+    private int GetNewRandom() {
+        if (this.randomSequence.Count == 0) {
+            this.randomSequence = new List<int>();
+            for (int i = 0; i < this.prefabs.Count; i++) {
+                this.randomSequence.Add(i);
+            }
+
+            Shuffle(this.randomSequence, 20);
+            //myList.Sort((a, b) => 1 - 2 * Random.Range(0, 1)); //myList.Shuffle();//OrderBy(x => Random.value).ToList();
+        }
+        var randomIndex = Random.Range(0, this.randomSequence.Count);
+        var randomObjectId = this.randomSequence[randomIndex];
+        this.randomSequence.RemoveAt(randomIndex);
+
+        return randomObjectId;
+
+    }
+
+    private void Shuffle(List<int> in_list, int steps) {
+        for (int i = 0; i < steps; i++) {
+            var fromIndex = Random.Range(0, in_list.Count);
+            var toIndex = Random.Range(0, in_list.Count);
+
+            var fromValue = in_list[fromIndex];
+            var toValue = in_list[toIndex];
+
+            in_list[fromIndex] = toValue;
+            in_list[toIndex] = fromValue;
+        }
     }
 }
