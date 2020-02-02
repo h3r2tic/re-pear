7d79938938b56ef821dc7c05bcafdcec3d7486e1
realpearson <david@wearemachinemedia.com>
2020-02-02 12:21:05 +0100

    :sound:

--- Assets/Scripts/LandSound.cs
+++ Assets/Scripts/LandSound.cs
@@ -7,19 +7,15 @@ public class LandSound : MonoBehaviour
     public bool isGrounded = false;
     public bool playOnce = true;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.tag == "Ground") 
-        {
-            isGrounded = true;
 
-            if (playOnce) 
-            {
-                Debug.Log("Land");
-                GetComponent<SimpleSoundModule>().PlayModule();
-                playOnce = false;
-            }
-            
+
+    private void Update()
+    {
+        if (playOnce && isGrounded)
+        {
+            Debug.Log("Land");
+            GetComponent<SimpleSoundModule>().PlayModule();
+            playOnce = false;
         }
     }
 }
---
+++ Assets/Scripts/PistonCol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonCol : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            GetComponentsInParent<LandSound>()[0].isGrounded = true;
+
+            Debug.Log("pist");
+
+        }
+    }
+}
